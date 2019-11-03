8e48b0e6164c80de1c2610b5c03e43e52c0c5ab7
Maike Rodrigues <maikerodrigues@localhost.localdomain>
2019-11-03 13:24:02 -0300

    Upload de images

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,30 @@ const express = require('express');
 const homeController = require('../controllers/homeController');
 const userController = require('../controllers/userController');
 const postController = require('../controllers/postController');
+
+const imageMiddleware = require('../middlewares/imageMiddleware');
+
 const router = express.Router();
 
-router.get('/',homeController.userMiddleWare,homeController.index);
+router.get('/',
+    homeController.userMiddleWare,
+    homeController.index
+);
 router.get('/users/login',userController.login);
 
 router.get('/post/add',postController.add);
-router.post('/post/add',postController.addAction);
+router.post('/post/add',
+    imageMiddleware.upload,
+    imageMiddleware.resize,
+    postController.addAction
+);
 
 router.get('/post/:slug/edit',postController.edit);
-router.post('/post/:slug/edit',postController.editAction);
+router.post('/post/:slug/edit',
+    imageMiddleware.upload,
+    imageMiddleware.resize,
+    postController.editAction
+);
 
 router.get('/post/:slug',postController.view);
 
