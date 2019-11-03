32101deeab3f91b35b4889db9eddfa009ddc2d01
Maike Rodrigues <maikerodrigues@localhost.localdomain>
2019-11-03 13:48:44 -0300

    Slug Único com Regex

--- models/Post.js
+++ models/Post.js
@@ -19,9 +19,21 @@ const postSchema = new mongoose.Schema({
 
 });
 
-postSchema.pre('save', function(next){
+postSchema.pre('save', async function(next){
     if(this.isModified('title')){
         this.slug = slug(this.title, {lower:true});
+
+        // EXPRESSAO REGULAR - Verificar a variacao de slug com a mesma expressao.
+        const slugRegex = new RegExp(`^(${this.slug})((-[0-9]{1,}$)?)$`,'i');
+
+        // Chama a consulta pelo contrutur da classe.
+        const postWithSlug = await this.constructor.find({slug:slugRegex});
+
+        // Atualiza o Slug com a quantidade existente mais um.
+        if(postWithSlug.length > 0){
+            this.slug = `${this.slug}-${postWithSlug.length + 1}`;
+        }
+
     }
 
     next();
